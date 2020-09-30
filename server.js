21966cf0d5b58140b10bcebc7772f3bcf3309095
Joona Somerkivi <joona.somerkivi@nordcloud.com>
2020-09-30 15:31:10 +0300

    Customer project modifications

--- server.js
+++ server.js
@@ -1,69 +1,90 @@
 var express = require('express');
-var passport = require('passport');
-
-var BearerStrategy = require("passport-azure-ad").BearerStrategy;
+var msal = require('@azure/msal-node');
+const jwt = require("jsonwebtoken");
 
 const tenantID = "2bb82c64-2eb1-43f7-8862-fdc1d2333b50";
 const clientID = "c49d3833-ee3c-4bfa-8fa8-d7566b7ec0fd";
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
 
-var options =  {
-  identityMetadata: `https://login.microsoftonline.com/${tenantID}/v2.0/.well-known/openid-configuration`,
-  clientID: clientID,
-  issuer: `https://sts.windows.net/${tenantID}/`,
-  audience: "http://customappsso/1a2607b0-c1c4-45db-b094-253173f3cef1",
-  loggingLevel: "info",
-  passReqToCallback: false
+const SERVER_PORT = process.env.PORT || 3000;
+
+// Before running the sample, you will need to replace the values in the config, 
+// including the clientSecret
+const config = {
+  auth: {
+    clientId: clientID,
+    authority: `https://login.microsoftonline.com/${tenantID}`,
+    clientSecret: "FILL IN YOUR SECRET"
+    //clientSecret:"",
+  },
+  system: {
+    loggerOptions: {
+      loggerCallback(loglevel, message, containsPii) {
+        console.log(message);
+      },
+      piiLoggingEnabled: false,
+      logLevel: msal.LogLevel.Verbose,
+    }
+  }
 };
 
-var bearerStrategy = new BearerStrategy(options, function(token, done) {
-  done(null, {}, token);
+// Create msal application object
+const pca = new msal.ConfidentialClientApplication(config);
+//const pca = new msal.PublicClientApplication(config);
+
+// Create Express App and Routes
+const app = express();
+
+app.get('/', (req, res) => {
+  const authCodeUrlParameters = {
+    //scopes: ["openid", `${clientID}/.default`],
+    //scopes: ["openid", "profile", "User.Read", `${clientID}/.default`],
+    scopes: ["openid", "profile", "User.Read", "Group.Read.All" ],
+    redirectUri: "http://localhost:3000/callback",
+  };
+
+  // get url to sign user in and consent to scopes needed for applicatio
+  pca.getAuthCodeUrl(authCodeUrlParameters).then((response) => {
+    console.log("Redirect url", response);
+    res.redirect(response);
+  }).catch((error) => {
+    console.log(JSON.stringify(error));
+    res.status(500).json(error);
+  });
 });
 
-var app = express();
-app.use(require('morgan')('combined'));
-app.use(require('cookie-parser')());
-app.use(require('body-parser').urlencoded({ extended: true }));
-app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
-app.use(passport.initialize());
-passport.use(bearerStrategy);
-
-// Enable CORS for * because this is a demo project
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Authorization, Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
-
-// This is where your API methods are exposed
-app.get(
-  "/api",
-  passport.authenticate("oauth-bearer", { session: false }),
-  function (req, res) {
-    var claims = req.authInfo;
-    console.log("User info: ", req.user);
-    console.log("Validated claims: ", claims);
-    res.status(200).json({ name: claims["name"] });
-  }
-);
-app.get(
-  "/callback",
-  function (req, res) {
-    console.log("We have a callback", { query: req.query });
-    var { error = "" } = req.query;
-    if (error) {
-      console.error(error);
-    }
+app.get('/callback', (req, res) => {
+  const tokenRequest = {
+    code: req.query.code,
+    scopes: ["openid"],
+    redirectUri: "http://localhost:3000/callback",
+  };
+  console.log({ code: req.query.code });
+  pca.acquireTokenByCode(tokenRequest).then((response) => {
+    console.log("AccessToken aquired");
     res.status(200).json(
-      { ...req.query }
+      { ...response }
     );
-  }
-);
-
-// Run this
-var port = process.env.PORT || 3000;
-app.listen(port, function() {
-  console.log("Listening on port " + port);
+  }).catch((error) => {
+    console.log(error);
+    res.status(500).json(error);
+  });
 });
+
+app.get('/check', (req, res) => {
+  try {
+    const { authorization } = req.headers;
+    const token = authorization.split(" ", 2)[1];
+    console.log({ authorization });
+    res.status(200).json(
+      jwt.decode(token)
+    )
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+app.listen(SERVER_PORT, () => console.log(`Msal Node Auth Code Sample app listening on port ${SERVER_PORT}!`))
